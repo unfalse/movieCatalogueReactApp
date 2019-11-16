3e6a6d0a9a7b90ccd2799053ed34a23a2efb26d3
Nopefish <fadeout01@gmail.com>
2019-11-16 21:12:17 +0500

    Search component  - added bulma styles for input  - markup and styles for search component  - added project settings for vscode

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -9,13 +9,38 @@ const Search = ({ onSearch }) => {
     const { searchParam } = getQueryParams(location);
     const onInputChange = e => {
         const searchValue = e.target.value;
-        history.push(`${getQueryParamsString({ newSearch: searchValue, newPage: 1 }, location)}`);
+        history.push(
+            `${getQueryParamsString(
+                { newSearch: searchValue, newPage: 1 },
+                location
+            )}`
+        );
         onSearch();
     };
     return (
-        <div>
-            Search by Title
-            <input type="text" onChange={onInputChange} value={searchParam} />
+        <div className="columns">
+            <div className="column is-narrow">
+                <div className="field is-horizontal box" style={{ width: 90 }}>
+                    <div className="field-label is-normal">
+                        <label className="label">Search by Title</label>
+                    </div>
+                </div>
+            </div>
+
+            <div className="column">
+                <div className="field-body">
+                    <div className="field">
+                        <div className="control">
+                            <input
+                                className="input"
+                                type="text"
+                                onChange={onInputChange}
+                                value={searchParam}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
