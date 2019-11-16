da38c13325911e3c27160e5e8fc30e98070da7e7
Nopefish <fadeout01@gmail.com>
2019-11-16 22:36:15 +0500

    Footer and Header  - extracted Footer and Header into separate files  - commented out the changes to bulma variables which came from the article about the bulma  - removed styles from App styles  - styled Footer  - styled Header (WIP)

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Footer = () => (
+    <footer className="footer">
+        <div className="content has-text-centered">
+            <div className="footer__icons-notice">
+                <p>
+                    Icons made by{' '}
+                    <a
+                        href="https://www.flaticon.com/authors/smashicons"
+                        title="Smashicons"
+                    >
+                        Smashicons
+                    </a>{' '}
+                    from{' '}
+                    <a href="https://www.flaticon.com/" title="Flaticon">
+                        www.flaticon.com
+                    </a>
+                </p>
+            </div>
+            <div>(c) 2019 by nopefish</div>
+        </div>
+    </footer>
+);
+
+export { Footer };
