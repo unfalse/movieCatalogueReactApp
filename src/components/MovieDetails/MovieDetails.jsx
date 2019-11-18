a1514ee5562cc0ff2de88017e1f1e22bfc9e0c20
Nopefish <fadeout01@gmail.com>
2019-11-19 03:54:38 +0500

    MovieDetails design  - centered and placed some info above the poster

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,58 +1,62 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 
-import {fetchMovie} from '../../apis';
+import { fetchMovie } from '../../apis';
 
-import {ImgFallback} from '../ImgFallback';
+import { ImgFallback } from '../ImgFallback';
 import NoPoster from '../../assets/noposter.png';
 
 import './MovieDetails.css';
 
-const MovieDetails = ({match, history}) => {
-  const {id} = match.params;
-  const [movie, setMovie] = useState({});
-  const goBack = () => {
-    history.goBack();
-  }
+const MovieDetails = ({ match, history }) => {
+    const { id } = match.params;
+    const [movie, setMovie] = useState({});
+    const goBack = () => {
+        history.goBack();
+    };
 
-  useEffect(() => {
-    fetchMovie(id).then(movieData => {
-      setMovie(movieData);
-    });
-  }, []);
+    useEffect(() => {
+        fetchMovie(id).then(movieData => {
+            setMovie(movieData);
+        });
+    }, []);
 
-  const {
-    posterUrl = '',
-    title = '',
-    genres = [],
-    year = '',
-    director = '',
-    plot = '',
-    actors = '',
-    runtime = '',
-  } = movie;
-  return (
-    <div>
-      <button onClick={goBack}>Go back</button>
-      <div className="movie-details">
-        <div className="movie-details__poster">
-          <ImgFallback alt="Movie poster" src={posterUrl} srcFallback={NoPoster}/>
+    const {
+        posterUrl = '',
+        title = '',
+        genres = [],
+        year = '',
+        director = '',
+        plot = '',
+        actors = '',
+        runtime = '',
+    } = movie;
+    return (
+        <div>
+            <button onClick={goBack}>Go back</button>
+            <div className="movie-details center">
+                <div className="title">{title}</div>
+                <div className="movie-details__genres">
+                    {genres.map((g, i) => (
+                        <span key={i}>{g}</span>
+                    ))}
+                </div>
+                <div className="movie-details__year">{year}</div>
+                <div className="movie-details__poster">
+                    <ImgFallback
+                        alt="Movie poster"
+                        src={posterUrl}
+                        srcFallback={NoPoster}
+                    />
+                </div>
+                <div className="movie-details__text-part">
+                    <div className="movie-details__director">{director}</div>
+                    <div className="movie-details__actors">{actors}</div>
+                    <div className="movie-details__plot">{plot}</div>
+                    <div className="movie-details__runtime">{runtime} min.</div>
+                </div>
+            </div>
         </div>
-        <div className="movie-details__text-part">
-          <div className="movie-details__title">{title}</div>
-          <div className="movie-details__genres">
-            {genres.map((g, i) => (
-              <span key={i}>{g}</span>
-            ))}
-          </div>
-          <div className="movie-details__year">{year}</div>
-          <div className="movie-details__director">{director}</div>
-          <div className="movie-details__actors">{actors}</div>
-          <div className="movie-details__plot">{plot}</div>
-          <div className="movie-details__runtime">{runtime} min.</div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
-export {MovieDetails};
+export { MovieDetails };
