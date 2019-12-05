8f2598cb7d389f2d5c71b888ad6a1fe1c1c25707
Nopefish <fadeout01@gmail.com>
2019-12-05 17:24:44 +0500

    Deploying to Netlify  - a tiny change to test github webhook

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
     <footer className="footer">
         <div className="content has-text-centered">
-            <div>(c) 2019 by nopefish</div>
+            <div>(c) 2019 by Nopefish</div>
             <div>Powered by Bulma.css</div>
         </div>
     </footer>
