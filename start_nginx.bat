687a3acb0765b72af99b6ca6a5fa8f6c3b2ac937
Nopefish <fadeout01@gmail.com>
2019-12-12 08:34:54 +0500

    Add nginx as development server  - untracked files

--- start_nginx.bat
+++ start_nginx.bat
@@ -1,4 +1,3 @@
 set nginx_path=c:\nginx-1.17.6
-echo %nginx_path%
 copy /y nginx_conf\movieReactAppLocal.conf %nginx_path%\conf\movieReactAppLocal.conf
 start /d %nginx_path% nginx.exe -c conf\movieReactAppLocal.conf
---
+++ stop_nginx.bat
@@ -0,0 +1,2 @@
+set nginx_path=c:\nginx-1.17.6
+start /d %nginx_path% nginx.exe -s stop
